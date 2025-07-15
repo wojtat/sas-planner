423de90f0c588a7a5625f0454f4ec86cbd65b3bf
Vojtěch Tilhon <wojtat@email.cz>
2025-07-15 19:37:00 +0200

    SAS: Mark some methods of the SAS parser private

--- sas.py
+++ sas.py
@@ -4,7 +4,7 @@ class SasParser:
         with open(sas_file_name, 'rt') as f:
             self.lines = list(f)
 
-    def eat_line(self):
+    def _eat_line(self):
         if self.lines:
             line = self.lines[0].strip()
             self.lines = self.lines[1:]
@@ -12,93 +12,93 @@ class SasParser:
         else:
             return ''
 
-    def parse_header(self):
-        assert self.eat_line() == 'begin_version'
-        version_number = int(self.eat_line())
-        assert self.eat_line() == 'end_version'
-        assert self.eat_line() == 'begin_metric'
-        uses_action_costs = self.eat_line() == '1'
-        assert self.eat_line() == 'end_metric'
+    def _parse_header(self):
+        assert self._eat_line() == 'begin_version'
+        version_number = int(self._eat_line())
+        assert self._eat_line() == 'end_version'
+        assert self._eat_line() == 'begin_metric'
+        uses_action_costs = self._eat_line() == '1'
+        assert self._eat_line() == 'end_metric'
         return version_number, uses_action_costs
 
-    def parse_variables(self):
-        num_variables = int(self.eat_line())
+    def _parse_variables(self):
+        num_variables = int(self._eat_line())
         for var_index in range(num_variables):
-            assert self.eat_line() == 'begin_variable'
-            _var_name = self.eat_line()
-            _axiom_layer = self.eat_line()
-            var_range = int(self.eat_line())
+            assert self._eat_line() == 'begin_variable'
+            _var_name = self._eat_line()
+            _axiom_layer = self._eat_line()
+            var_range = int(self._eat_line())
             for i in range(var_range):
-                _value_name = self.eat_line()
-            assert self.eat_line() == 'end_variable'
+                _value_name = self._eat_line()
+            assert self._eat_line() == 'end_variable'
         return num_variables
 
-    def parse_mutex_groups(self):
-        num_mutex_groups = int(self.eat_line())
+    def _parse_mutex_groups(self):
+        num_mutex_groups = int(self._eat_line())
         for i in range(num_mutex_groups):
-            assert self.eat_line() == 'begin_mutex_group'
-            num_facts = int(self.eat_line())
+            assert self._eat_line() == 'begin_mutex_group'
+            num_facts = int(self._eat_line())
             for j in range(num_facts):
-                self.eat_line()
-            assert self.eat_line() == 'end_mutex_group'
+                self._eat_line()
+            assert self._eat_line() == 'end_mutex_group'
 
-    def parse_initial_state(self, num_variables):
+    def _parse_initial_state(self, num_variables):
         initial_values = []
-        assert self.eat_line() == 'begin_state'
+        assert self._eat_line() == 'begin_state'
         for i in range(num_variables):
-            initial_values.append(int(self.eat_line()))
-        assert self.eat_line() == 'end_state'
+            initial_values.append(int(self._eat_line()))
+        assert self._eat_line() == 'end_state'
         return initial_values
 
-    def parse_goal_state(self):
-        assert self.eat_line() == 'begin_goal'
-        num_assignments = int(self.eat_line())
+    def _parse_goal_state(self):
+        assert self._eat_line() == 'begin_goal'
+        num_assignments = int(self._eat_line())
         assignments = []
         for i in range(num_assignments):
-            var_index, value = self.eat_line().split(' ')
+            var_index, value = self._eat_line().split(' ')
             var_index = int(var_index)
             value = int(value)
             assignments.append((var_index, value))
-        assert self.eat_line() == 'end_goal'
+        assert self._eat_line() == 'end_goal'
         return assignments
 
-    def parse_actions(self):
-        num_actions = int(self.eat_line())
+    def _parse_actions(self):
+        num_actions = int(self._eat_line())
         actions = []
         for i in range(num_actions):
-            assert self.eat_line() == 'begin_operator'
-            action_name = self.eat_line()
+            assert self._eat_line() == 'begin_operator'
+            action_name = self._eat_line()
 
-            num_prevailing_var = int(self.eat_line())
+            num_prevailing_var = int(self._eat_line())
             prevailing_vars = []
             for j in range(num_prevailing_var):
-                var_index, value = self.eat_line().split(' ')
+                var_index, value = self._eat_line().split(' ')
                 var_index = int(var_index)
                 value = int(value)
                 prevailing_vars.append((var_index, value))
 
-            num_effect_var = int(self.eat_line())
+            num_effect_var = int(self._eat_line())
             effect_vars = []
             for j in range(num_effect_var):
-                _, effected_var, changed_from, changed_to = self.eat_line().split(' ')
+                _, effected_var, changed_from, changed_to = self._eat_line().split(' ')
                 effected_var = int(effected_var)
                 changed_from = int(changed_from)
                 changed_to = int(changed_to)
                 effect_vars.append((effected_var, changed_from, changed_to))
 
-            action_cost = int(self.eat_line())
-            assert self.eat_line() == 'end_operator'
+            action_cost = int(self._eat_line())
+            assert self._eat_line() == 'end_operator'
             actions.append((action_name, action_cost, prevailing_vars, effect_vars))
         return actions
 
     def parse(self):
-        version_number, uses_action_costs = self.parse_header()
+        version_number, uses_action_costs = self._parse_header()
         assert version_number == 3
-        num_variables = self.parse_variables()
-        self.parse_mutex_groups()
-        initial_values = self.parse_initial_state(num_variables)
-        goal_state = self.parse_goal_state()
-        actions = self.parse_actions()
+        num_variables = self._parse_variables()
+        self._parse_mutex_groups()
+        initial_values = self._parse_initial_state(num_variables)
+        goal_state = self._parse_goal_state()
+        actions = self._parse_actions()
         return num_variables, initial_values, goal_state, actions
 
 
